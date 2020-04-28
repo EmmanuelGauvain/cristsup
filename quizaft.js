86cbc7132b4d8bba5865239a572e9ed1f8bae74b
EmmanuelGauvain <58739530+EmmanuelGauvain@users.noreply.github.com>
2020-04-28 09:07:22 +0200

    Add files via upload

---
+++ quizaft.js
@@ -0,0 +1,12 @@
+
+
+
+var option2 = getElementById("option2").value
+
+document.getElementById("resultat").innerHTML = "mmh";
+document.getElementById("resultat").innerHTML = option2;
+
+
+function sowhat() {
+    document.getElementById("resultat").innerHTML = option2
+}
